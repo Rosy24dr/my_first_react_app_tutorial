063e3a090adfd5b99e207558fed3e47897026f10
rossebell <Rosy24dr@outlook.com>
2022-04-16 22:45:40 -0400

    created the search.svg file and entered the information provided, created the title MovieLand on the website and I am able to search for the movie

--- src/app.js
+++ src/app.js
@@ -1,26 +1,35 @@
 import React from "react";
-import {useEffect} from "react";
+import { useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
 
-//c291179f
-
-const API_URL = 'http://www.omdbapi.com?apikey=c291179f';
+const API_URL = "http://www.omdbapi.com?apikey=c291179f";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
+    console.log(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
 
-        console.log(data.Search);
-    }
-    useEffect(() => {
-        searchMovies('Spiderman')
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
 
-    }, []);
-
-    return (
-        <h1>App</h1>
-    )
-}
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" />
+      </div>
+    </div>
+  );
+};
 
 export default App;
