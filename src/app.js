39ed942824bfb1b588de118041e011b93bc225e4
rossebell <Rosy24dr@outlook.com>
2022-04-16 22:08:16 -0400

    created the App.css file, renamed the app.js file to App.js, added the css code provided, created the App function to search for movies and display the output

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 import React from "react";
+import {useEffect} from "react";
+
+//c291179f
+
+const API_URL = 'http://www.omdbapi.com?apikey=c291179f';
+
+const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+    useEffect(() => {
+        searchMovies('Spiderman')
+
+    }, []);
+
+    return (
+        <h1>App</h1>
+    )
+}
+
+export default App;
