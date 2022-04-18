17718af66231aef7507638516c20083f29cade4a
rossebell <Rosy24dr@outlook.com>
2022-04-17 20:41:01 -0400

    finished the movies conditional and the search bar

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
@@ -15,11 +15,13 @@ const movie1 = {
 };
 
 const App = () => {
+  const [movies, setMovies] = useState();
+  const [searchTerm, setSearchTerm]= useState('')
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   };
   useEffect(() => {
     searchMovies("Spiderman");
@@ -32,15 +34,24 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
       </div>
 
-      <div className="container">
-        <MovieCard movie1={movie1}/>
-      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+
     </div>
   );
 };
